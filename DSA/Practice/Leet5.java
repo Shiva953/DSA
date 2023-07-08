5ccc93a605b9b10a42f84bb52d1fc57276de1d7c
Shiva953 <b22070@students.iitmandi.ac.in>
2023-07-08 22:48:29 +0530

    .

--- DSA/Practice/Leet5.java
+++ DSA/Practice/Leet5.java
@@ -1,7 +1,46 @@
+// https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
 package DSA.Practice;
 
 public class Leet5 {
     public static void main(String[] args) {
-        
+        int[][] arr = {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
+        System.out.println(countNegatives(arr));
+    }
+    private static int countNegatives(int[][] grid) {
+        int c = 0,start,end;
+        for(int i=0;i<grid.length;i++){
+            start = 0;
+            end = grid[i].length-1;
+            if(end==1){
+                if(grid[i][0]<0 && grid[i][1]<0){
+                    c += 2;
+                }
+                else if(grid[i][0]<0 || grid[i][1]<0){
+                    c += 1;
+                }
+                else{
+                    c += 0;
+                }
+            }
+            else{
+                if(grid[i][end]>=0){
+                    end += 1;
+                }
+                else{
+                while(start<end){
+                    int mid = start + (end-start)/2;
+                    if(grid[i][mid]>=0){
+                        start = mid + 1;
+                    }
+                    else if(grid[i][mid]<0){
+                        end = mid;
+                    }
+                }
+                }
+            c += grid[i].length - end;
+            }
+        }
+        return c;
     }
 }
--- DSA/Practice/Leet6.java
+++ DSA/Practice/Leet6.java
@@ -1,7 +1,28 @@
+// https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+
 package DSA.Practice;
 
 public class Leet6 {
-    public static void main(String[] args) {
-        
+    int m, n;
+    public int kthSmallest(int[][] matrix, int k) {
+        m = matrix.length; n = matrix[0].length; // For general, the matrix need not be a square
+        int start = matrix[0][0], end = matrix[m-1][n-1], ans = -1;
+        while (start <= end) {
+            int mid = (start + end) >> 1;
+            if (countLessOrEqual(matrix, mid) >= k) {
+                ans = mid;
+                end = mid - 1; // try to looking for a smaller value in the start side
+            } else start = mid + 1; // try to looking for a bigger value in the end side
+        }
+        return ans;
+    }
+    int countLessOrEqual(int[][] matrix, int x) {
+        int cnt = 0, c = n - 1; // start with the endmost column
+        for (int r = 0; r < m; ++r) {
+            while (c >= 0 && matrix[r][c] > x)
+            --c;  // decrease column until matrix[r][c] <= x
+            cnt += (c + 1);
+        }
+        return cnt;
     }
 }
